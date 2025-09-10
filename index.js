b11db1a15ca2895a6e5efbeeb0189e441abe161f
Shreyash Damodar <shreyashdamodar1086@gmail.com>
2025-09-10 11:41:08 +0530

    created the end points

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 const express = require('express')
+
+const app = express();
+
+app.use(express.json())
+
+app.post('/user/signup', (req, res)=> {
+    res.json({
+        message: "Signup Endpoint"
+    })
+})
+
+app.post('/user/signup', (req, res) => {
+    res.json({
+        message: "Signin Endpoint"
+    })
+})
+
+
+app.get('/user/purchases', (req, res) => { //give user courses
+    res.json({
+        message: "Signin Endpoint"
+    })
+})
+
+app.post('/course/purchase', (req, res) => { //give user courses
+    res.json({
+        message: "Signin Endpoint"
+    })
+})
+
+
+app.get('/courses', (req, res) => { //give all the courses
+    res.json({
+        message: "Signin Endpoint"
+    })
+})
+
+app.listen(3000)
